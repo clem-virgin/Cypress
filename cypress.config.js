37fd04ddd02a22d7252204fb87320f5058d4dbe5
clem-virgin <clementvirgin02@gmail.com>
2026-03-17 15:32:27 -0400

    first commit

--- cypress.config.js
+++ cypress.config.js
@@ -4,6 +4,7 @@ module.exports = defineConfig({
   allowCypressEnv: false,
 
   e2e: {
+    chromeWebSecurity:false,
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
