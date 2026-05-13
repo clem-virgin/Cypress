e73617511cca6166691ec45b7b339824466f7421
clem-virgin <clementvirgin02@gmail.com>
2026-05-13 07:52:24 -0400

    first commit

--- cypress.config.js
+++ cypress.config.js
@@ -5,7 +5,7 @@ module.exports = defineConfig({
 
   e2e: {
    // baseUrl: "https://parabank.parasoft.com/parabank",
-   // baseUrl: "https://www.saucedemo.com/",
+    baseUrl: "https://www.saucedemo.com/",
     chromeWebSecurity: false,
     setupNodeEvents(on, config) {
       // implement node event listeners here
